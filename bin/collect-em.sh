4775da090791daedb019676787c20ba032fed146
Darius Bacon <darius@wry.me>
2017-09-22 19:59:09 -0700

    add -traditional-cpp to cpp command line so that tests run again

--- bin/collect-em.sh
+++ bin/collect-em.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 for f in fib sieve random rev copy wc qsort sha1
 do 
-    cpp benchmarks/$f.idel | ./idelasm | ./profiling-idelvm >/dev/null
+    cpp -traditional-cpp benchmarks/$f.idel | ./idelasm | ./profiling-idelvm >/dev/null
     mv -i opcode_profile profile.$f
 done
--- bin/experiment.sh
+++ bin/experiment.sh
@@ -2,7 +2,7 @@
 
 rm opcode_profile
 for f in fib sieve random rev copy wc qsort sha1
-do cpp benchmarks/$f.idel | ./idelasm | ./profiling-idelvm >/dev/null
+do cpp -traditional-cpp benchmarks/$f.idel | ./idelasm | ./profiling-idelvm >/dev/null
 done
 
 for f in 0 50 100 150 200 250 300 350 400
